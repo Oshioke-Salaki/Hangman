fea5895326ec184c748dff0104fc9886aae2f087
Oshioke-Salaki <salaki1902@gmail.com>
2023-01-16 21:18:37 -0600

    implemented winning and losing feature

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Hangman
 
-My very wn hangman game built using reactJS
+My very own hangman game built using reactJS.
+
+# How it works
+
+On start, you have 6 guesses in order to guess the correct letters that make up the hidden word.
+
+# Hosted version
+
+Here is the link to the hosted version: https://hangv1.netlify.app/
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import "./App.css";
-import Hangman from "./Hangman";
+import React, { Component } from 'react';
+import './App.css';
+import Hangman from './Hangman';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Hangman />
+        <p>Created by Oshioke Salaki &copy; 2023</p>
       </div>
     );
   }
